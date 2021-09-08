ef9df969bf2561e971fe56daba2deaaffad37254
kameoman <spiderman.935963@gmail.com>
2021-09-08 18:41:50 +0900

    main

---
+++ Access.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Access {
+    public static void main(String[] args){
+    if(args.length != 1){
+      System.out.println("ファイル名を指定してください");
+      System.exit(1);
+    }
+    try{
+      BufferedReader br = new BufferedReader(new FileReader(args[0]));
+
+      String str;
+      while((str = br.readLine()) != null){
+        System.out.println(str);
+      }
+      br.close();
+    }
+    catch(IOException e){
+      System.out.println("入出力エラーです。");
+    }
+  }
+}
