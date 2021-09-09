d69e82b0b8104976d23385dafa6fb9d919e9f6f1
kameoman <spiderman.935963@gmail.com>
2021-09-09 09:24:48 +0900

    split

--- Access.java
+++ Access.java
@@ -11,10 +11,38 @@ public class Access {
 
       String str;
       while((str = br.readLine()) != null){
-        String[]target = new String[5];
-        target[0] = str.substring(53,72);
-        System.out.println(target[0]);
-        System.out.println(str);
+        // 文字列の分割
+        String regex = "/";
+        String strtry = str;
+
+        String[] result = str.split(regex,0);
+        for (int i= 0; i < result.length; i++){
+          // System.out.println("["+result[i]+"]");
+          String path = "["+result[i]+"]";
+          System.out.println(path);
+        }
+
+        System.out.println("-- --");
+
+        // result = str.split(regex, -1);
+        // for (int i = 0 ; i < result.length; i++){
+        //   System.out.println("[" + result[i] + "]");
+        // }
+
+        // System.out.println("-- --");
+
+        // result = str.split(regex, 2);
+        // for (int i = 0 ; i < result.length; i++){
+        //   System.out.println("[" + result[i] + "]");
+        // }
+
+
+
+        // String[]target = new String[5];
+        // target[0] = str.substring(53,72);
+        // System.out.println(target[0]);
+        // // System.out.println(target[1]);
+        // System.out.println(str);
       }
       br.close();
     }
