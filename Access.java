7927edefe3404954c4cf61ed63d9a5c9645f0cfc
kameoman <spiderman.935963@gmail.com>
2021-09-09 10:09:44 +0900

    toArray

--- Access.java
+++ Access.java
@@ -1,4 +1,10 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
+import javax.naming.StringRefAddr;
 
 public class Access {
     public static void main(String[] args){
@@ -9,19 +15,33 @@ public class Access {
     try{
       BufferedReader br = new BufferedReader(new FileReader(args[0]));
 
-      String str;
+      String str = null;
       while((str = br.readLine()) != null){
+        // 文字列を1行ずつに変換する
+        // System.out.println(">>> " + str);
+        // リスト作成
+        List<String> list1 = new ArrayList<>();
         // 文字列の分割
         String regex = "/";
         String strtry = str;
 
         String[] result = str.split(regex,0);
-        for (int i= 0; i < result.length; i++){
-          // System.out.println("["+result[i]+"]");
-          String path = "["+result[i]+"]";
-          System.out.println(path);
+        Collections.addAll(list1, result);
+        for (String a : list1){
+          System.out.println("["+a+"]");
+          System.out.println("-- --");
         }
 
+        // for (int i= 0; i < result.length; i++){
+        //   // System.out.println("["+result[i]+"]");
+        //   String path = "["+result[i]+"]";
+        //   System.out.println(path);
+        // }
+
+        // System.out.println(result.length);
+        String url_path = result[3]+"/"+result[4];
+        String[] ab = new String[] {url_path};
+        System.out.println(Arrays.toString(ab));
         System.out.println("-- --");
 
         // result = str.split(regex, -1);
