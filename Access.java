2463477e3326f946429be81e206ac0b557c44827
kameoman <spiderman.935963@gmail.com>
2021-09-09 11:22:47 +0900

    for line_data

--- Access.java
+++ Access.java
@@ -15,7 +15,7 @@ public class Access {
       System.exit(1);
     }
     // ファイルを1行ずつ配列に入れる
-    String[] tempInside = null;
+    String[] line_by_line_data = null;
     try{
       BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
       List<String> lineList = new ArrayList<String>();
@@ -23,22 +23,26 @@ public class Access {
       while((line = br.readLine()) != null){
         lineList.add(line);
       }
-      tempInside = lineList.toArray(new String[lineList.size()]);
+      line_by_line_data = lineList.toArray(new String[lineList.size()]);
             List<String> list1 = new ArrayList<>();
               // // 文字列の分割（各行ごとに分割）
               String regex = "/";
-              String strtry = tempInside[1];
-      
-              String[] result = strtry.split(regex,0);
-              Collections.addAll(list1, result);
-              for (String a : list1){
-                System.out.println("["+a+"]");
+                    // 各行ごとに必要な項目を取り出してくる
+              for (String str: line_by_line_data){
+                String[] result = str.split(regex,0);
+                Collections.addAll(list1, result);
+                for (String a : list1){
+                  // System.out.println("["+a+"]");
+                  System.out.println(result[3]+"/"+result[4]);
+                }
                 System.out.println("-- --");
+                
               }
+      
       // 配列の追加
       // String stringArray=result[3]+"/"+result[4];
       String[] access_path = new String[5];
-      access_path[0] = result[3]+"/"+result[4];
+      // access_path[0] = result[3]+"/"+result[4];
       access_path[1] = "add";
       System.out.println(Arrays.toString(access_path));
       System.out.println(access_path[0]);
