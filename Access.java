493735fbe16544079100742e4ff856d3bb706337
kameoman <spiderman.935963@gmail.com>
2021-09-09 11:02:40 +0900

    array

--- Access.java
+++ Access.java
@@ -22,51 +22,6 @@ public class Access {
       String line;
       while((line = br.readLine()) != null){
         lineList.add(line);
-
-        // // リスト作成
-        // List<String> list1 = new ArrayList<>();
-        // // 文字列の分割
-        // String regex = "/";
-        // String strtry = tempInside[1];
-
-        // String[] result = strtry.split(regex,0);
-        // Collections.addAll(list1, result);
-        // for (String a : list1){
-        //   System.out.println("["+a+"]");
-        //   System.out.println("-- --");
-        // }
-
-        // for (int i= 0; i < result.length; i++){
-        //   // System.out.println("["+result[i]+"]");
-        //   String path = "["+result[i]+"]";
-        //   System.out.println(path);
-        // }
-
-        // System.out.println(result.length);
-        // String url_path = result[3]+"/"+result[4];
-        // String[] ab = new String[] {url_path};
-        // System.out.println(Arrays.toString(ab));
-        // System.out.println("-- --");
-
-        // result = line.split(regex, -1);
-        // for (int i = 0 ; i < result.length; i++){
-        //   System.out.println("[" + result[i] + "]");
-        // }
-
-        // System.out.println("-- --");
-
-        // result = line.split(regex, 2);
-        // for (int i = 0 ; i < result.length; i++){
-        //   System.out.println("[" + result[i] + "]");
-        // }
-
-
-
-        // String[]target = new String[5];
-        // target[0] = line.substring(53,72);
-        // System.out.println(target[0]);
-        // // System.out.println(target[1]);
-        // System.out.println(line);
       }
       tempInside = lineList.toArray(new String[lineList.size()]);
             List<String> list1 = new ArrayList<>();
@@ -80,7 +35,14 @@ public class Access {
                 System.out.println("["+a+"]");
                 System.out.println("-- --");
               }
-      System.out.println(result[3]+"/"+result[4]);
+      // 配列の追加
+      // String stringArray=result[3]+"/"+result[4];
+      String[] access_path = new String[5];
+      access_path[0] = result[3]+"/"+result[4];
+      access_path[1] = "add";
+      System.out.println(Arrays.toString(access_path));
+      System.out.println(access_path[0]);
+      System.out.println(access_path[1]);
       br.close();
     }
     catch(IOException e){
