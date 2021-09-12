fc0814fe068337264edecc27bda7f2c365dd4c65
kameoman <spiderman.935963@gmail.com>
2021-09-12 09:26:50 +0900

    OsAccess-comp

--- OsAccess.java
+++ OsAccess.java
@@ -10,27 +10,27 @@ public class OsAccess {
       System.exit(1);
     }
     // ファイルを1行ずつ配列に入れる
-    String[] line_by_line_data = null;
+    String[] Os_Access_data = null;
     try{
-      BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+      BufferedReader base_data_read = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
       List<String> lineList = new ArrayList<String>();
       String line;
-      while((line = br.readLine()) != null){
+      while((line = base_data_read.readLine()) != null){
         lineList.add(line);
       }
-      line_by_line_data = lineList.toArray(new String[lineList.size()]);
+      Os_Access_data = lineList.toArray(new String[lineList.size()]);
               // // 文字列の分割（各行ごとに分割）
-              int line_count = line_by_line_data.length;
+              int line_count = Os_Access_data.length;
               String[] access_path = new String[line_count];
 
               // 各行ごとに必要な項目を取り出してくる
               for (int i=0; i<line_count; i++){
                 // 条件を付けて文字を抽出
                 String beginStr = "Mozilla";
-                int beginIndex = line_by_line_data[i].indexOf(beginStr);
+                int beginIndex = Os_Access_data[i].indexOf(beginStr);
                 String endStr = ")";
-                int endIndex = line_by_line_data[i].indexOf(endStr) + endStr.length();
-                access_path[i] = line_by_line_data[i].substring(beginIndex-1, endIndex);
+                int endIndex = Os_Access_data[i].indexOf(endStr) + endStr.length();
+                access_path[i] = Os_Access_data[i].substring(beginIndex-1, endIndex);
                 }
               Integer[] access_count_list = new Integer[line_count];
               // 要素ごとに個数計測
@@ -88,7 +88,7 @@ public class OsAccess {
               System.out.println("MacOSの割合"+(double)Math.round(mac_rate* 10)/10+"%");
               double other_rate = (total_other / total_os) *100;
               System.out.println("Windows、MacOS以外の割合"+(double)Math.round(other_rate* 10)/10+"%");
-              br.close();
+              base_data_read.close();
     }
     catch(IOException e){
       System.out.println("入出力エラーです。");
